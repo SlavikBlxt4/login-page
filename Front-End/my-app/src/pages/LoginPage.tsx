f4c6fa391f7bf68d569a343a24502801eb54e900
SlavikBlxt4 <158159514+SlavikBlxt4@users.noreply.github.com>
2024-03-08 22:31:22 +0100

    commit

--- Front-End/my-app/src/pages/LoginPage.tsx
+++ Front-End/my-app/src/pages/LoginPage.tsx
@@ -5,7 +5,15 @@ const LoginPage: React.FC<LoginPageProps> = ({ setPage }) => {
     return (
       <div>
         <div className='login-box'>
-          <div className='login-title'><h1>Login</h1></div>
+          <div className='login-title'>
+            <h1>Login</h1>
+            <p>Enter your email and password</p>
+          </div>
+
+          <div className='login-input'><input type="email" placeholder="Email" /></div>
+          <div className='login-input'><input type="password" placeholder="Password" /></div>
+          <button className='login-button' onClick={() => setPage('/')}>Login</button>
+
         </div>
       </div>
     );
